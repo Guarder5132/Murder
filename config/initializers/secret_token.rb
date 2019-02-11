c0c58e2bbbce88a6e22bda519965413af9f618db
Guarder <1772107801@qq.com>
2019-02-11 10:07:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MurderApp::Application.config.secret_key_base = '28522d76d958247b9d32f91ee2203208b6b5ff94553f52c75c7a54dedfd479582575787cb6fbff68471ebb3e5cf34dcca3156384cb30d3523922ab9900ffcc07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MurderApp::Application.config.session_store :cookie_store, key: '_Murder_app_session'
